b63f4d8c4ee62250e2b293f9ff0fd1585e53cb06
Michael Plata <michaelplata@Michaels-MacBook-Pro.local>
2022-09-02 11:03:02 +0100

    adds heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 app.use(cors())
-// app.use(express.static('build'))
+app.use(express.static('build'))
 // const Note = require('./models/note')
 
 let notes = [
