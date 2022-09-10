30fa06a16fc69ec154178f800220ea6b1231ae31
Michael Plata <michaelplata@Michaels-MacBook-Pro.local>
2022-09-10 10:59:58 +0100

    adds put method

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ app.use(requestLogger)
 
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
-    console.log('notes:', notes);
     response.json(notes)
   })
 })
@@ -30,14 +29,7 @@ app.get('/api/notes/:id', (request,response) => {
     })
 })
 
-// app.delete('/api/notes/:id', (request, response) => {
-//     const id = Number(request.params.id)
-//     notes = notes.filter(note => note.id !== id)
-
-//     response.status(204).end()
-// })
-
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
 
     if(body.content === undefined){
@@ -54,13 +46,35 @@ app.post('/api/notes', (request, response) => {
     note.save().then(savedNote => {
         response.json(savedNote)
     })
+    .catch(error => next(error))
   })
 
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  app.put('/api/notes/:id', (request, response, next) => {
+    const { content, important } = request.body
+  
+    Note.findByIdAndUpdate(
+      request.params.id, 
+      { content, important },
+      { new: true, runValidators: true, context: 'query' }
+    ) 
+      .then(updatedNote => {
+        response.json(updatedNote)
+      })
+      .catch(error => next(error))
+  })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if(error.name === 'CastError'){
+    return response.status(400).send({error: 'malformatted id'})
+      } else if (error.name === 'ValidationError'){
+        return response.status(400).json({error: error.message})
+      }
+      next(error)
   }
 
-app.use(unknownEndpoint)
+
+app.use(errorHandler)
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
